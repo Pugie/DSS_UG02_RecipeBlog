15b4a82afad9c4decb82870b26286f803e32d295
Rho Bates <rhobates@outlook.com~>
2026-04-22 12:04:57 +0100

    changing around my tests, you can run them and get errors by using npm test

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+import SignUp from '../app/public/js/signup.js';
+var test_SignUp = new SignUp;
+
+describe("Testing User Sign Up", function (){
+
+    it("Test should not allow blank username", function(){
+        testuser = new SignUp();
+        
+    });
+
+});
